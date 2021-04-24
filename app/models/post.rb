fd679994159abad89aadad4ca3a4272505ce925c
Christerle Nounoute <christerlesn@gmail.com>
2021-04-24 05:08:34 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
   validates_with TitleValidator
-  validates_with CategoryValidator
   validates :content, length: { minimum: 100 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
    message: "%{value} is not a valid category" }, allow_nil: true
