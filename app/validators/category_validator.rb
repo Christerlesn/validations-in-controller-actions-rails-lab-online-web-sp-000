cf1ecda1362193179b0de0713d00532ae250935c
Christerle Nounoute <christerlesn@gmail.com>
2021-04-24 04:52:36 +0000

    Automatically backed up by Learn

--- app/validators/category_validator.rb
+++ app/validators/category_validator.rb
@@ -1,7 +1,7 @@
 class CategoryValidator < ActiveModel::Validator
 
   def validate(record)
-    unless record.category.empty? == nil
+    unless record.category == nil
       record.errors[:category] << "Need to have a category"
     end
   end
