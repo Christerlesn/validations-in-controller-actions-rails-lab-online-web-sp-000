9ce9bc90c9490c8424464ff71613246a64c00919
Christerle Nounoute <christerlesn@gmail.com>
2021-04-24 04:47:51 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   include ActiveModel::Validations
   validates_with TitleValidator
   validates :content, length: { minimum: 10 }
-  validates :category, length: { minimum: 1 }
+  # validates :category, length: { minimum: 1 }
 end
