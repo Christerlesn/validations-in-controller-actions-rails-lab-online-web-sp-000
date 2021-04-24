f3cd01ecfbcd70d55d68f080bf16937896aaf0c7
Christerle Nounoute <christerlesn@gmail.com>
2021-04-24 05:03:51 +0000

    Automatically backed up by Learn

--- app/validators/category_validator.rb
+++ app/validators/category_validator.rb
@@ -1,9 +1,9 @@
 class CategoryValidator < ActiveModel::Validator
 
    def validate(record)
-    unless record.category.include?("Fiction") || record.category.include?("Non-Fiction")
-      record.category.valid? == false
-      record.errors[:category] << "Need to have a category"
-    end
+    # unless record.category.include?("Fiction") || record.category.include?("Non-Fiction")
+    #   record.category.valid? == false
+    #   record.errors[:category] << "Need to have a category"
+    # end
    end
 end
