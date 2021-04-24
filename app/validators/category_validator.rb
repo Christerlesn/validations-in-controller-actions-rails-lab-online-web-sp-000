b1a924e284abea12a0e61e34307c061e4713d79d
Christerle Nounoute <christerlesn@gmail.com>
2021-04-24 04:48:31 +0000

    Automatically backed up by Learn

---
+++ app/validators/category_validator.rb
@@ -0,0 +1,8 @@
+class CategoryValidator < ActiveModel::Validator
+
+  def validate(record)
+    unless record.category != []
+      record.errors[:category] << "Need to have a category"
+    end
+  end
+end
